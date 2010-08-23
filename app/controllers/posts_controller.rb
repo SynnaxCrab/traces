da57115080ef4aa858db324bb6ac2fb5b043d37f
Winfield <winfield301@gmail.com>
2010-08-23 11:05:35 +0800

    init the project

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traces::Application.config.secret_token = 'c26ae781721dd17d187c375c0ca141aa91b0c181daacdcb218a630e8baa99d453b9e802af0f810bf9bfceb60f181f6eccef1338770026c3957332bd9be3f7f56'
