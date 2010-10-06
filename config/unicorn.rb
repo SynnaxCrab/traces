e34119f398aa5d7c82ec641a27191e2a0c801bea
Winfield <winfield301@gmail.com>
2010-10-06 15:36:22 +0800

    modify the unicorn configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,8 @@
-listen 2010
+listen 2010, :tcp_nopush => true
+timeout 30
 worker_processes 4 # this should be >= nr_cpus
+working_directory "/home/winfield/lainuo.info/http/traces/current"
+
 pid "/home/winfield/lainuo.info/http/traces/shared/pids/unicorn.pid"
-stderr_path "/home/winfield/lainuo.info/http/traces/shared/log/unicorn.log"
-stdout_path "/home/winfield/lainuo.info/http/traces/shared/log/unicorn.log"
+stderr_path "/home/winfield/lainuo.info/http/traces/shared/log/unicorn.stderr.log"
+stdout_path "/home/winfield/lainuo.info/http/traces/shared/log/unicorn.stdout.log"
