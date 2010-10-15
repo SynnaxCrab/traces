fbee6fc5f225ef10d1b9807ddc35187d8f09c38a
Winfield <winfield301@gmail.com>
2010-10-15 20:55:44 +0800

    add application_controller.rb back and remove tiny_mce, will use markdown

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.0.0'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3-ruby', :require => 'sqlite3'
-gem 'tiny_mce'
+# gem 'tiny_mce'
 gem 'will_paginate', '~> 3.0.pre2'
 
 # Use unicorn as the web server
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  
+  USERNAME, PASSWORD = "lainuo", "h123456" 
+  
+  def post_slug(post)
+    date_array = post.created_at.to_time.to_formatted_s(:db).split(' ').first.split('-')
+    "/" + date_array.join("/") + "/" + post.id.to_s + "-" + post.title.parameterize
+  end
+  
+  protected
+  
+  def authenticate 
+    authenticate_or_request_with_http_basic do |username, password| 
+      username == USERNAME &&  
+      password == PASSWORD 
+      end 
+  end 
+end
