430a2fb6242eddd5d534362463919b780c11c1f5
Winfield <winfield301@gmail.com>
2011-06-17 22:58:08 +0800

    redirect visitors using IE below 9 to browser suggesion page

--- app/controllers/backbone_controller.rb
+++ app/controllers/backbone_controller.rb
@@ -2,12 +2,11 @@ class BackboneController < ApplicationController
   #layout nil
   layout :determine_layout
   def index
-    #@ua = request.user_agent
-    #@ua = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
-    @ua = "Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US)"
-    
-    if is_IE_below_9? @ua
-      puts "blocked"
+    #ua = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
+    #ua = "Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US)"
+    #ua = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    if is_IE_below_9?
+      @is_ie_9 = true
       render :file => "#{Rails.root}/public/browsers.html", :layout => false
     end
     
@@ -22,18 +21,18 @@ class BackboneController < ApplicationController
   
   private
   def is_googlebot? 
-    ua = request.user_agent   
-    puts ua
+    ua = request.user_agent
     #ua = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    
     return false if ua.nil?
     return false unless ua.downcase.index('googlebot')
     
     true
   end
   
-  def is_IE_below_9?(ua)
-    puts "kkkk"
-    puts ua
+  def is_IE_below_9?
+    ua = request.user_agent
+    #ua = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
     
     return false if ua.nil?
     if ua.downcase.index('msie')
