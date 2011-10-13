3667a67c1b12888f28dbb40c71515dd81f8013fe
Winfield <winfield301@gmail.com>
2011-10-13 22:28:05 +0800

    add compression and static resource cache

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'rack/cache'
+
+Traces::Application.configure do
+  # Enable compression
+  config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
+  
+  # Enable Static resources Cache
+  # $cache = Memcache.new
+  $cache = Dalli::Client.new('localhost:11211')
+  config.middleware.use Rack::Cache, :metastore => $cache, :entitystore => 'file:tmp/cache/entity'
+end
 
 # Initialize the rails application
 Traces::Application.initialize!
