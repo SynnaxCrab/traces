cdcf1c1f9f86f71c96e3e0b7e11253858fdde331
Winfield <winfield301@gmail.com>
2012-08-19 00:54:25 +0800

    Autoload lib/ folder including all subdirectories

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Traces
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    # Autoload lib/ folder including all subdirectories
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
