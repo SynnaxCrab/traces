dc58ab686ac95b2977fb2a734a58abf0fff955c2
Winfield <winfield301@gmail.com>
2013-08-05 14:24:46 +0800

    Refactor articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,19 @@
 class ArticlesController < ApplicationController
+  respond_to :html
   layout :layout_by_google_analytics
-  before_filter :authenticate_user!, :except => [:index, :show, :show_redirect, :show_all, :feed, :more]
+  before_action :authenticate_user!, :except => [:index, :show, :show_redirect, :show_all, :feed, :more]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    skip = params[:skip].nil? ? 0 : params[:skip]
-    @articles = Article.by_published_at.descending.limit(5).skip(skip).all
-    respond_to do |format|
-      format.html
-      format.json { render json: @articles}
-    end
-    #render :json => @articles
+    #TODO: make limit configurable || params[:limit]
+    @articles = Article.by_published_at.descending.limit(5).skip(params[:skip].to_i)
+    respond_with(@articles)
   end
 
   def drafts
-    @articles = Article.by_saved_at.descending.limit(10).all
-    respond_to do |format|
-      format.html
-      format.json { render json: @articles}
-    end
+    #TODO: make limit configurable || params[:limit]
+    @articles = Article.by_saved_at.descending.limit(10)
+    respond_with(@articles)
   end
 
   def new
@@ -25,66 +21,46 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new_by_user(params[:article], params[:commit], current_user)
-    if @article.save
-      flash[:notice] = "Article was successfully created !"
-      #TODO save as draft should not just redirect to @article
-      redirect_to @article
-    else
-      render :new
-    end
+    @article = Article.new(params[:article])
+    @article.author = current_user.id
+    @article.is_draft = params[:commit] == "Save" ? true : false
+    @article.save
+    respond_with(@article)
   end
 
   def edit
-    @article = Article.by_slug.key(params[:id]).first
   end
 
   def update
-    @article = Article.by_slug.key(params[:id]).first
-
-    if @article.update_attributes(params[:article])
-      flash[:notice] = "Article was successfully updated !"
-      redirect_to @article
-    else
-      render :edit
-    end
+    @article.update_attributes(params[:article])
+    respond_with(@article)
   end
 
   def show
-    @article = Article.by_slug.key(params[:id]).first
-    #redirect_to article_slug(@article)
-    respond_to do |format|
-      format.html { redirect_or_render_404(@article) }
-      format.json { render json: @article }
+    if @article.is_draft
+      @comments = Article.by_comments_article_created_at.
+        startkey([@article.id]).
+        endkey([@article.id, Time.now]).
+        rows
+
+      respond_with(@article)
+    else
+      redirect_to article_slug(@article)
     end
   end
 
-  def show_redirect
-    @article = Article.by_slug.key(params[:id]).first
-    #redirect_to article_slug(@article)
-    redirect_or_render_404(@article)
-  end
-
   def show_all
+    @articles = Article.articles_by_time_slug(params)
 
-    @articles = Article.articles_by_time_slug(params, @begin_time, @end_time).all
-
-    if @articles.size == 1
+    if @articles.count == 1
       @article = @articles.first
       @comments = Article.by_comments_article_created_at.
         startkey([@article.id]).
         endkey([@article.id, Time.now]).
-        all
+        rows
     end
 
-    if (@article.nil? || @article.empty?) && (@articles.nil? || @articles.empty?)
-      render :file => "#{Rails.root}/public/404.html", :layout => false, :status => 404
-      return
-    end
-
-    respond_to do |format|
-      format.html
-    end
+    render_404 if @article.blank? && @articles.blank?
   end
 
   def feed
@@ -100,4 +76,9 @@ class ArticlesController < ApplicationController
      format.atom
     end
   end
+
+  private
+    def set_article
+      @article = Article.by_slug.key(params[:id]).first
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,20 +3,5 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  include FactoryGirl::Syntax::Methods
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  # fixtures :all
-
-  # Add more helper methods to be used by all tests here...
-  @@CouchDB.default_database.recreate!
-
-  FACTORIES_PATH = File.join(File.dirname(__FILE__), '/factories')
-
-  def article_slug(article)
-    date_array = article.created_at.to_time.to_formatted_s(:db).split(' ').first.split('-')
-    "/" + date_array.join("/") + "/" + article.slug #title.parameterize
-  end
+  Article.database.recreate!
 end
